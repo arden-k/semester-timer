fc4c6e46a6eddd290b536fdcbdc593002ce291e6
Arden <arden.eb.kim@gmail.com>
2025-09-08 14:18:29 +0900

    first

---
+++ time.js
@@ -0,0 +1,24 @@
+const year = new Date().getFullYear();
+const myDate = new Date("11 30, 2025 21:00:00");
+console.log(myDate);
+
+// countdown
+let timer = setInterval(function () {
+  // get today's date
+  const today = new Date().getTime();
+
+  // get the difference
+  const diff = myDate - today;
+
+  // math
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  // display
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}, 1);
